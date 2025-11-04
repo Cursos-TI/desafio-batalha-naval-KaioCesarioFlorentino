ddd275e7a6f510024cd0d7f34f13bbd884d51dff
KaioCesarioFlorentino <kaiocesario2005@gmail.com>
2025-11-04 11:08:38 +0000

    desafio aventureiro finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+
 int main() {
     char linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int colunaH = 2, linhaH = 1;
-    int colunaV = 5, linhaV = 7;
+    int linhaH = 1, colunaH = 2;
+    int linhaV = 7, colunaV = 5;
+    int linhaD1 = 7, colunaD1 = 7;
+    int linhaD2 = 3, colunaD2 = 1;
     int navio = 3;
     int numeroLinha = 1;
 
     //Inicializando o tabuleiro (Matriz 10x10)
-    int tabuleiro[10][10];
+    int tabuleiro[LINHAS][COLUNAS];
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             tabuleiro[i][j] = 0;
@@ -23,6 +28,7 @@ int main() {
         }
     } else {
         printf("Erro: o navio horizontal não cabe no tabuleiro!\n");
+        return 0;
     }
 
     // Posicionando o navio vertical (3 casas)
@@ -40,7 +46,54 @@ int main() {
         } 
     } else {
         printf("Erro: o navio horizontal não cabe no tabuleiro!\n");
+        return 0;
     }
+
+
+    // Posicionando o navio diagonal 1 (3 casas)
+    //Verifica se tem sobreposição
+    // Verifica se cabe no tabuleiro
+    if(linhaD1 + 3 <= 10){
+        if(colunaD1 + 3 <= 10){
+            for(int i = 0; i < 3; i++){
+                    if(tabuleiro[linhaD1 + i][colunaD1 + i] == 0){
+                        tabuleiro[linhaD1 + i][colunaD1 + i] = navio;
+                    } else{
+                        printf("Aviso! Tentativa de sobreposisção detectada, altere os navios de posição.\n");
+                        return 0;
+                    }
+                }
+            } else{
+                printf("Erro: o navio diagonal não cabe no tabuleiro!\n");
+                return 0;
+            }
+        } else{
+            printf("Erro: o navio diagonal não cabe no tabuleiro!\n");
+            return 0;
+        }
+
+    // Posicionando o navio diagonal 2 (3 casas)
+    //Verifica se tem sobreposição
+    // Verifica se cabe no tabuleiro
+    if(linhaD2 + 3 <= 10){
+        if(colunaD2 + 3 <= 10){
+            for(int i = 0; i < 3; i++){
+                    if(tabuleiro[linhaD2 + i][colunaD2 + i] == 0){
+                        tabuleiro[linhaD2 + i][colunaD2 + i] = navio;
+                    } else{
+                        printf("Aviso! Tentativa de sobreposisção detectada, altere os navios de posição.\n");
+                        return 0;
+                    }
+                }
+            } else{
+                printf("Erro: o navio diagonal não cabe no tabuleiro!\n");
+                return 0;
+            }
+        } else{
+            printf("Erro: o navio diagonal não cabe no tabuleiro!\n");
+            return 0;
+        }
+    
 
     //Exibindo o tabuleiro
     printf("\n=====  TABULEIRO DE BATALHA NAVAL   =====\n\n");
